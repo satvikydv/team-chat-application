deac53e6aea07fd9260762c5c5e0c9781b447a60
Satvik Yadav <satvikydv@gmail.com>
2024-09-02 23:38:32 +0530

    dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -20,8 +22,20 @@ export default function RootLayout({
     <ClerkProvider
     afterSignOutUrl={"/"}
     >
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(
+          font.className,
+          "bg-white dark:bg-[#202020]"
+          )}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="tca-theme"
+          >
+            {children}
+          </ThemeProvider>
+          </body>
       </html>
     </ClerkProvider>
   );
