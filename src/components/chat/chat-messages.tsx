25fba292700986be2bb37d6a8e11c5f1d531034d
Satvik Yadav <satvikydv@gmail.com>
2024-11-02 16:36:38 +0530

    chat item component

--- src/components/chat/chat-messages.tsx
+++ src/components/chat/chat-messages.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import React from "react";
-import { Member } from "@prisma/client";
+import React, { Fragment } from "react";
+import { Member, Message, Profile } from "@prisma/client";
 import { Loader2, ServerCrash } from "lucide-react";
+import { format } from "date-fns";
 
 import { ChatWelcome } from "@/components/chat/chat-welcome";
+import { ChatItem } from "@/components/chat/chat-item";
 import { useChatQuery } from "@/hooks/use-chat-query";
 
 interface ChatMessagesProps {
@@ -18,6 +20,14 @@ interface ChatMessagesProps {
   paramValue: string;
   type: "channel" | "conversation";
 }
+
+type MessagesWithMemberWithProfile = Message & {
+  member: Member & {
+    profile: Profile;
+  };
+};
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 export function ChatMessages({
   name,
@@ -49,20 +59,44 @@ export function ChatMessages({
       </div>
     );
 
-    if (status === "error")
-        return (
-          <div className="flex flex-col flex-1 justify-center items-center">
-            <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
-            <p className="text-xs text-zinc-500 dark:text-zinc-400">
-              Something went wrong...
-            </p>
-          </div>
-        );
+  if (status === "error")
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Something went wrong!
+        </p>
+      </div>
+    );
 
   return (
     <div className="flex-1 flex flex-col py-4 overflow-y-auto">
       <div className="flex-1" />
       <ChatWelcome name={name} type={type} />
+      <div className="flex flex-col-reverse mt-auto">
+        {data?.pages.map((group, index) => (
+          <Fragment key={index}>
+            {group?.items.map((message: MessagesWithMemberWithProfile) => (
+              <ChatItem
+                key={message.id}
+                currentMember={member}
+                member={message.member}
+                id={message.id}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(
+                  new Date(message.createdAt),
+                  DATE_FORMAT
+                )}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketQuery={socketQuery}
+                socketUrl={socketUrl}
+              />
+            ))}
+          </Fragment>
+        ))}
+      </div>
     </div>
   );
 }
